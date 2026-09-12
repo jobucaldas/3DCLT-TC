f75e7c8d0ad8913824b7697b185e5639b922614a
João Bueno <joaovictorbuenodecaldas@gmail.com>
2026-09-12 14:25:08 -0300

    refactor: move argo definition to terraform

--- terraform/modules/argo/main.tf
+++ terraform/modules/argo/main.tf
@@ -8,3 +8,35 @@ resource "helm_release" "argocd" {
   wait             = true
   timeout          = 600
 }
+
+resource "kubernetes_manifest" "app" {
+  for_each = var.apps
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = each.key
+      namespace = "argocd-${var.environment}"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = var.repo_url
+        targetRevision = var.target_revision
+        path           = "k8s/app/${each.key}"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = each.value
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = ["CreateNamespace=true"]
+      }
+    }
+  }
+  depends_on = [helm_release.argocd]
+}
--- terraform/modules/argo/variables.tf
+++ terraform/modules/argo/variables.tf
@@ -1,4 +1,22 @@
 variable "environment" { type = string }
+variable "repo_url" {
+  type    = string
+  default = "https://github.com/jobucaldas/3DCLT-TC.git"
+}
+variable "target_revision" {
+  type    = string
+  default = "t3"
+}
+variable "apps" {
+  type = map(string)
+  default = {
+    auth-service       = "togglemaster-auth"
+    flag-service       = "togglemaster-flag"
+    targeting-service  = "togglemaster-targeting"
+    evaluation-service = "togglemaster-evaluation"
+    analytics-service  = "togglemaster-analytics"
+  }
+}
 variable "chart_version" {
   type    = string
   default = "10.9.0"
